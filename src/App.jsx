d1dc3c95ef5b45de4548f05efeb8369572c86051
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-22 22:12:38 -0800

    chore: uninstall aos library

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./css/index.css";
-import "aos/dist/aos.css";
-import AOS from "aos";
-import Landing from "./sections/Landing";
-import AboutMe from "./sections/AboutMe";
-import Projects from "./sections/Projects";
-import OtherProjects from "./sections/OtherProjects";
-import Skills from "./sections/Skills";
-import Contact from "./sections/Contact";
-import Footer from "./sections/Footer";
+// import "aos/dist/aos.css";
+// import AOS from "aos";
+// import Landing from "./sections/Landing";
+// import AboutMe from "./sections/AboutMe";
+// import Projects from "./sections/Projects";
+// import OtherProjects from "./sections/OtherProjects";
+// import Skills from "./sections/Skills";
+// import Contact from "./sections/Contact";
+// import Footer from "./sections/Footer";
 
 function App() {
   React.useEffect(() => {
