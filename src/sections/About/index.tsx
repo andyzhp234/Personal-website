8376995409a6c27c72d426f7c1aff2814111ca6c
Haopeng Zeng <andyzhp234@gmail.com>
2023-03-24 01:27:53 -0700

    chore: update image assets

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SectionContainer from "../../components/SectionContainer";
 import SectionTitle from "../../components/SectionTitle";
-import SelfImage from "../../assets/images/me.png";
+import SelfImage from "../../assets/images/me1.jpg";
 import { fadeUpObserver } from "../../utils/scrollAnimation";
 
 export default function About() {
@@ -18,7 +18,7 @@ export default function About() {
         ref={textRef}
       >
         <div className="flex w-full justify-center lg:w-1/2">
-          <img className="w-56" src={SelfImage} alt="self-image" />
+          <img className=" w-96 rounded-xl" src={SelfImage} alt="self-image" />
         </div>
         <div className="sm:2xl mt-10 w-full text-center text-xl md:text-3xl lg:mt-0 lg:w-1/2 lg:text-left xl:text-5xl">
           My expertise is developing intuitive and scalable software
