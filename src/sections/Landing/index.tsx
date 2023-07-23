a2999135ebae3ae57544c9ce5fe8957983d02e80
andy <andyzhp234@gmail.com>
2023-07-22 20:52:11 -0700

    style(UI): update landing page spacing

--- src/sections/Landing/index.tsx
+++ src/sections/Landing/index.tsx
@@ -10,20 +10,22 @@ type LandingProps = {
 
 export default function Landing({ scrollToTarget }: LandingProps) {
   return (
-    <SectionContainer>
-      <SelfIntro />
-      <div className="unreveal flex items-center py-5">
-        <PrimaryButton
-          onClick={() => scrollToTarget("contact")}
-          type="tertiary"
-        >
-          Hire Me
-        </PrimaryButton>
-        <NavigationItem onClick={() => scrollToTarget("project")}>
-          See my works
-        </NavigationItem>
-        <NavigationItem isDownload={true}>My Resume</NavigationItem>
-      </div>
-    </SectionContainer>
+    <div className="mb-40 mt-20 sm:ml-5 md:ml-10 lg:ml-14 xl:ml-20">
+      <SectionContainer>
+        <SelfIntro />
+        <div className="unreveal flex items-center py-5">
+          <PrimaryButton
+            onClick={() => scrollToTarget("contact")}
+            type="tertiary"
+          >
+            Hire Me
+          </PrimaryButton>
+          <NavigationItem onClick={() => scrollToTarget("project")}>
+            See my works
+          </NavigationItem>
+          <NavigationItem isDownload={true}>My Resume</NavigationItem>
+        </div>
+      </SectionContainer>
+    </div>
   );
 }
