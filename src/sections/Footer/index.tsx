b0b7fae6d818d3f558b09ba95e57b224ad4d0e0d
Haopeng Zeng <62565095+andyzhp234@users.noreply.github.com>
2024-01-08 21:12:49 -0800

    fix(footer): update copyright year to 2023

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <h1 className="w-full py-4 text-center text-xs sm:text-base" id="footer">
-      Haopeng Zeng @ 2023. All rights reserved.
+      Haopeng Zeng © 2023. All rights reserved.
     </h1>
   );
 }
