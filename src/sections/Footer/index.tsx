7471060afc37d74d7ff88040a0e021308abcb4e3
Nick Chen <haojungc@gmail.com>
2024-01-19 00:16:21 -0800

    fix(footer): update copyright year automatically

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear: number = new Date().getFullYear();
   return (
     <h1 className="w-full py-4 text-center text-xs sm:text-base" id="footer">
-      Haopeng Zeng © 2023. All rights reserved.
+      Haopeng Zeng © {currentYear}. All rights reserved.
     </h1>
   );
 }
